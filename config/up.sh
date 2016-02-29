d0b7aef95f2008dd0187ea91856074e98f84c04e
louisamoros <amoros.louis@gmail.com>
2016-02-29 17:17:44 +0100

    update up.sh

--- config/up.sh
+++ config/up.sh
@@ -1,13 +1,13 @@
 #!/bash/sh
 
 # docker compose create the docker-compose.yml to set dockers
-docker-compose create --force-recreate -d -p /config/docker-compose.yml
+docker-compose -p /config/docker-compose.yml create --force-recreate
 
 # add github repository to jdk8-mvn-cdb
 docker cp . jdk8-mvn-cdb:cdb
 
 # start building with docker compose
-docker-compose start -a -p /config/docker-compose.yml
+docker-compose -p /config/docker-compose.yml start -a
 
 # copy builded war
 docker cp jdk8-mvn-cdb:cdb/target/*.war webapp.war
